f316984a57958e6b068704cc4986bbc896df48cb
neophack <pep3309531@163.com>
2024-04-01 01:56:45 +0000

    Update opengl_setup_glfw.cpp
    work  on raspi

--- src/hello_imgui/internal/backend_impls/opengl_setup_helper/opengl_setup_glfw.cpp
+++ src/hello_imgui/internal/backend_impls/opengl_setup_helper/opengl_setup_glfw.cpp
@@ -31,9 +31,9 @@ namespace HelloImGui { namespace BackendApi
         {
             // GL 3.2+
             glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-            glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+            //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+            //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
         }
         #endif
     }
